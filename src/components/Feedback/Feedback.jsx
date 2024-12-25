04bcac658f30028076cdaa85c9a0f8713d4db3e8
Serhiy Malysh <166062683+finconsultsm@users.noreply.github.com>
2024-12-25 14:01:06 +0100

    4

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -3,19 +3,17 @@ import sc from "./Feedback.module.css";
 const Feedback = ({ options, totalFeedback, positiveFeedback }) => {
   const optionsBtn = Object.entries(options);
 
-  if (totalFeedback > 0) {
-    return (
-      <ul className={sc.list}>
-        {optionsBtn.map((option, index) => (
-          <li key={index}>
-            {option[0]}: {option[1]}
-          </li>
-        ))}
-        <li key={"total"}>Total: {totalFeedback}</li>
-        <li key={"positive"}>Positive: {positiveFeedback}%</li>
-      </ul>
-    );
-  }
+  return (
+    <ul className={sc.list}>
+      {optionsBtn.map((option, index) => (
+        <li key={index}>
+          {option[0]}: {option[1]}
+        </li>
+      ))}
+      <li key={"total"}>Total: {totalFeedback}</li>
+      <li key={"positive"}>Positive: {positiveFeedback}%</li>
+    </ul>
+  );
 };
 
 export default Feedback;
